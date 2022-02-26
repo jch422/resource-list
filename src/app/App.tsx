c8b05cfc44892b6b13aaa83ee1e9fd52b1709897
jc <jotace422@gmail.com>
2022-02-27 06:00:58 +0900

    chore: App.tsx를 app 디렉토리 아래로 이동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 
-import Sidebar from "./components/sidebar/Sidebar";
+import Sidebar from "../components/sidebar/Sidebar";
+import Main from "../components/main/Main";
 
 export interface ResourceState {
   resource: {
     id: string;
     value: string;
     type: "url" | "img";
+    isClicked: true | false;
   };
 }
 
 function App() {
   const [resources, setResources] = useState<ResourceState["resource"][]>([]);
+  const clickedResource = resources.filter((r) => r.isClicked)?.[0] || null;
 
   return (
     <>
       <Sidebar resources={resources} setResources={setResources} />
+      <Main resource={clickedResource} setResources={setResources} />
     </>
   );
 }
